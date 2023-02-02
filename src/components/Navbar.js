db848265b7fcae3cbabad45a3b26bc522235996a
Suhair <saljammal995@gmail.com>
2023-02-02 23:32:47 +0200

    design the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,21 @@
 import "./navbar.scss";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span>Daily Blog</span>
+        </div>
+        <div className="links">
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/single"}>Single</NavLink>
+          <NavLink to={"/login"}>Logout</NavLink>
+          <NavLink to={"/write"}>New Blog</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
