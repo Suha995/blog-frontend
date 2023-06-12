2d9ad80270246a032745aaa37a90b76ba8f69bdb
Suhair <saljammal995@gmail.com>
2023-06-12 18:07:44 +0300

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,19 @@ const Navbar = () => {
   const user = true;
   return (
     <div className="navbar">
-      <div className="container">
-        <div className="logo">
-          <span>Daily Blog</span>
-        </div>
-        <div className="links">
-          <NavLink to={"/"}>Home</NavLink>
-          {/* <NavLink to={"/single"}>Single</NavLink> */}
-          <NavLink to={"/write"}>New Blog</NavLink>
-          {user && <NavLink to={"/login"}>Logout</NavLink>}
-        </div>
+      <div className="links">
+        <NavLink to={"/"}>Blog</NavLink>
+        {/* <NavLink to={"/single"}>Single</NavLink> */}
+        <NavLink to={"/write"}>Write</NavLink>
+      </div>
+      <div className="logo">
+        <span>Daily Blog</span>
+      </div>
+      <div className="links">
+        <NavLink to={"/login"}>Log in</NavLink>
+        {/* <NavLink to={"/single"}>Single</NavLink> */}
+        <NavLink to={"/register"}>Register</NavLink>
+        {user && <NavLink to={"/login"}>Logout</NavLink>}
       </div>
     </div>
   );
