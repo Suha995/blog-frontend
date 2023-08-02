218b90471cd15075cf1b6e86ab69f5f2aede1dd5
Suhair <saljammal995@gmail.com>
2023-08-02 22:03:30 +0300

    register and login user with jwt

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,56 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passError, setPassError] = useState(null);
+  const [isError, setIsError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setEmailError(null);
+    setPassError(null);
+    setIsError(null);
+    try {
+      const res = await fetch("http://localhost:8000/api/auth/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.errors) {
+        setEmailError(data.errors.email);
+        setPassError(data.errors.password);
+        return;
+      }
+      window.location.href = "/";
+    } catch (err) {
+      console.log(err);
+      setIsError("Sth went wrong" + err);
+    }
+  };
   return (
     <div className="login">
       <h2>Login</h2>
-      <form>
-        <p>There is an error!</p>
+      {isError && <span>{isError}</span>}
+      <form onSubmit={handleSubmit}>
         <label>Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        {emailError && <div className="error">{emailError}</div>}
+
         <label>Password</label>
-        <input type="password" required />
+        <input
+          type="text"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        {passError && <div className="error">{passError}</div>}
         <button>Login</button>
         <span>
           Don't have an account? <Link to={"/register"}>Create one now.</Link>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,17 +1,60 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./login-register.scss";
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passError, setPassError] = useState(null);
+  const [isError, setIsError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setEmailError(null);
+    setPassError(null);
+    setIsError(null);
+    try {
+      const res = await fetch("http://localhost:8000/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.errors) {
+        setEmailError(data.errors.email);
+        setPassError(data.errors.password);
+        return;
+      }
+      window.location.href = "/";
+    } catch (err) {
+      console.log(err);
+      setIsError("Sth went wrong" + err);
+    }
+  };
+
   return (
     <div className="register">
       <h2>Register</h2>
-      <form>
-        <p>There is an error!</p>
+      {isError && <span>{isError}</span>}
+      <form onSubmit={handleSubmit}>
         <label>Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        {emailError && <div className="error">{emailError}</div>}
+
         <label>Password</label>
-        <input type="password" required />
-        <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="text"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        {passError && <div className="error">{passError}</div>}
         <button>Register</button>
         <span>
           Already Registered? <Link to={"/login"}>Log in now.</Link>
